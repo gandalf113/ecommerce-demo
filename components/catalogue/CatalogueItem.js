6e105b3468877c2c47ef076c236cb5db66e3e75f
Aleksander Szczepaniak <lequit7@gmail.com>
2022-08-29 10:27:40 +0200

    Added contact form

--- components/catalogue/CatalogueItem.js
+++ components/catalogue/CatalogueItem.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CatalogueItem = ({ item }) => {
   return (
-    <div className='group space-y-3 overflow-hidden '>
+    <div className='group space-y-1 overflow-hidden '>
       <div className='overflow-hidden '>
-        <img src={item.image} alt="" className='object-cover  group-hover:scale-105 duration-200' />
+        <img src={item.image} alt={item.name} className='object-cover  group-hover:scale-105 duration-200'/>
       </div>
       <div>
         <p className='text-sm font-light group-hover:underline underline-offset-2'>{item.name}</p>
